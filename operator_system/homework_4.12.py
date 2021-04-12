ae29de416244c0ba1a3776a865975275a16e40bc
fzj <>
2021-04-12 18:54:48 +0800

    4.12操作系统作业修订2

--- operator_system/homework_4.12.py
+++ operator_system/homework_4.12.py
@@ -1,3 +1,4 @@
+import math
 if __name__ == '__main__':
      #path = "hightemp.txt"
     path = "hightemp - 副本.txt"
@@ -11,11 +12,24 @@ if __name__ == '__main__':
         contents = file.readlines()
 
 
-    file_list = list(contents)
+
 
     N = int(input())
-    for i in range(len(file_list) // N):
-        if((i+1)*N >= len(file_list)):
-            print(file_list[i * N: len(file_list)])
+    files = []
+    for i in range(math.ceil(len(contents) / N)):
+        if((i+1)*N >= len(contents)):
+            files.append(contents[i * N: len(contents)])
+
         else:
-            print(file_list[i * N: (i + 1) * N])
+            files.append(contents[i * N: (i + 1) * N])
+
+
+    for i in range(len(files)):
+        k = ""
+        for j in files[i]:
+            k = k + j
+
+        with open(str(i)+".txt", "w", encoding="UTF-8") as f:
+            f.write(k)
+
+
